d6ea27da280a1f1792466d3df368d34f480dbd39
ryankim86 <ryan.joon.kim@gmail.com>
2017-02-12 00:59:58 -0800

    Problem 1 got the regex stuff working to some extent

--- HW5/Ryan_Kim_HW5.py
+++ HW5/Ryan_Kim_HW5.py
@@ -6,6 +6,69 @@ Created on Sat Feb 11 19:01:52 2017
 """
 
 """
+Challenge 1
+"""
+def balance(eq):
+    import re
+    import sympy as sp
+    # split the equation into its two halves
+    equation = eq.split('=', 1)
+    print(equation[0])
+    print(equation[1])
+
+    # go through the input and find the elements involved
+    leftMatches = re.findall('([A-Za-z\d]+)', equation[0])
+    rightMatches = re.findall('([A-Za-z\d]+)', equation[1])
+    
+    Matches = re.findall('([A-Z]{1}[a-z]?)(\d)?', equation[0])
+    for match in Matches:
+        print(match)
+    
+    # create a list of symbols
+    n = len(leftMatches)+len(rightMatches)
+    symbolString = 'x0:' + str(n)
+    xList = sp.symbols(symbolString)
+    xList = ['x%d'%i for i in range(n)]
+    
+    # create a dictionary that will store the dictionaries to store components of the eq
+    left = {}
+    right = {}
+    
+    # parse through chemical blocks
+    for i in range(len(leftMatches)):
+        elementMatches = re.findall('([A-Z]{1}[a-z]?)(\d)?', leftMatches[i])
+        left['x%d'%i] = {}
+        for match in elementMatches:
+            if match[0] in left['x%d'%i]:
+                prevValue = left['x%d'%i][match[0]]
+                if len(match[1]) == 0:
+                    left['x%d'%i][match[0]] = prevValue + 1
+                else:
+                    left['x%d'%i][match[0]] = prevValue + int(match[1])
+            else:
+                if len(match[1]) == 0:
+                    left['x%d'%i][match[0]] = 1
+                else:
+                    left['x%d'%i][match[0]] = int(match[1])
+    for i in range(len(leftMatches), n-1):
+        elementMatches = re.findall('([A-Z]{1}[a-z]?)(\d)?', rightMatches[i])
+        right['x%d'%i] = {}
+        for match in elementMatches:
+            if match[0] in right['x%d'%i]:
+                prevValue = right['x%d'%i][match[0]]
+                if len(match[1]) == 0:
+                    right['x%d'%i][match[0]] = prevValue + 1
+                else:
+                    right['x%d'%i][match[0]] = prevValue + int(match[1])
+            else:
+                if len(match[1]) == 0:
+                    right['x%d'%i][match[0]] = 1
+                else:
+                    right['x%d'%i][match[0]] = int(match[1])
+    
+    return
+
+"""
 Challenge 3
 """
 def taylor_figure(k):
